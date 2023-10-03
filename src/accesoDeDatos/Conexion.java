b96717db7c1f46720fd6901037a0a8fb71bf5b13
ValeriaMonzon <141686216+ValeriaMonzon@users.noreply.github.com>
2023-10-03 18:24:49 -0300

    primera parte

---
+++ src/accesoDeDatos/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesoDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author valef
+ */
+public class Conexion {
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidad";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+
+    // controlar  cantidad de instancias de conexion
+    // con un constructor privado no se va a poder instanciar obtetos de ella desde afuera
+    private Conexion() {
+    }
+
+    public static Connection getConnection() {
+
+        if (connection == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los drivers");
+            }catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos");
+            }
+        }
+        return connection;
+    }
+    
+    
+}
