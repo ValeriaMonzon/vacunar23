a635b5907f09d8c3edc62c6f3f512a0b25634799
LucaDap2805 <lucapianojunior@gmail.com>
2023-10-04 20:58:15 -0300

    Revert "Merge branch 'main' of https://github.com/ValeriaMonzon/vacunar23"
    This reverts commit bad31583d95d9d0d4d3607b03c97c0eff930ca37, reversing
    changes made to 79da972a4508f852ba0e55e98b3bf84f7d901b6d.

--- src/entidades/Laboratorio.java
+++ src/entidades/Laboratorio.java
@@ -24,11 +24,6 @@ public class Laboratorio {
         this.pais = pais;
         this.domComercial = domComercial;
     }
-    
-    public Laboratorio(String cuit, String NomLaboratorio) {
-        this.cuit = cuit;
-        this.NomLaboratorio = NomLaboratorio;
-    }
 
     public String getCuit() {
         return cuit;
