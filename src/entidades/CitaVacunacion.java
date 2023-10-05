216c380c72dc0eac22f285592273a74a60a4742f
Elio Flores <eliofloreschoque2357@gmail.com>
2023-10-05 19:32:19 -0300

    update cita ciudadano data

--- src/entidades/CitaVacunacion.java
+++ src/entidades/CitaVacunacion.java
@@ -12,116 +12,113 @@ import java.time.LocalDateTime;
  * @author valef
  */
 public class CitaVacunacion {
-    private int CodCita;
-    private Ciudadano persona;
-    private int dni;
-    private int codRefuerzo;
-    private String FechaHoraCita;
-    private int centroVacunacion;
-    private LocalDateTime fechaHoraColoca;
-    private Vacuna dosis;
-    private boolean citaEstado;
 
-    public CitaVacunacion(int CodCita, Ciudadano persona, int codRefuerzo, String FechaHoraCita, int centroVacunacion, LocalDateTime fechaHoraColoca, Vacuna dosis, boolean citaEstado) {
-        this.CodCita = CodCita;
-        this.persona = persona;
-        this.codRefuerzo = codRefuerzo;
-        this.FechaHoraCita = FechaHoraCita;
-        this.centroVacunacion = centroVacunacion;
-        this.fechaHoraColoca = fechaHoraColoca;
-        this.dosis = dosis;
-        this.citaEstado = citaEstado;
-    }
+  private int CodCita;
+  private Ciudadano persona;
+  private int dni;
+  private int codRefuerzo;
+  private LocalDateTime FechaHoraCita;
+  private int centroVacunacion;
+  private LocalDateTime fechaHoraColoca;
+  private Vacuna dosis;
+  private boolean citaEstado;
 
-    public CitaVacunacion(int CodCita, Ciudadano persona, int dni, int codRefuerzo, String FechaHoraCita, int centroVacunacion, LocalDateTime fechaHoraColoca, Vacuna dosis, boolean citaEstado) {
-        this.CodCita = CodCita;
-        this.persona = persona;
-        this.dni = dni;
-        this.codRefuerzo = codRefuerzo;
-        this.FechaHoraCita = FechaHoraCita;
-        this.centroVacunacion = centroVacunacion;
-        this.fechaHoraColoca = fechaHoraColoca;
-        this.dosis = dosis;
-        this.citaEstado = citaEstado;
-    }
+  public CitaVacunacion(int CodCita, Ciudadano persona, int codRefuerzo, LocalDateTime FechaHoraCita, int centroVacunacion, LocalDateTime fechaHoraColoca, Vacuna dosis, boolean citaEstado) {
+    this.CodCita = CodCita;
+    this.persona = persona;
+    this.codRefuerzo = codRefuerzo;
+    this.FechaHoraCita = FechaHoraCita;
+    this.centroVacunacion = centroVacunacion;
+    this.fechaHoraColoca = fechaHoraColoca;
+    this.dosis = dosis;
+    this.citaEstado = citaEstado;
+  }
 
- 
-    public CitaVacunacion() {
-    }
+  public CitaVacunacion(int CodCita, Ciudadano persona, int dni, int codRefuerzo, LocalDateTime FechaHoraCita, int centroVacunacion, LocalDateTime fechaHoraColoca, Vacuna dosis, boolean citaEstado) {
+    this.CodCita = CodCita;
+    this.persona = persona;
+    this.dni = dni;
+    this.codRefuerzo = codRefuerzo;
+    this.FechaHoraCita = FechaHoraCita;
+    this.centroVacunacion = centroVacunacion;
+    this.fechaHoraColoca = fechaHoraColoca;
+    this.dosis = dosis;
+    this.citaEstado = citaEstado;
+  }
 
-    public int getCodCita() {
-        return CodCita;
-    }
+  public CitaVacunacion() {
+  }
 
-    public void setCodCita(int CodCita) {
-        this.CodCita = CodCita;
-    }
+  public int getCodCita() {
+    return CodCita;
+  }
 
-    public Ciudadano getPersona() {
-        return persona;
-    }
+  public void setCodCita(int CodCita) {
+    this.CodCita = CodCita;
+  }
 
-    public void setPersona(Ciudadano persona) {
-        this.persona = persona;
-    }
+  public Ciudadano getPersona() {
+    return persona;
+  }
 
-    public int getDni() {
-        return dni;
-    }
+  public void setPersona(Ciudadano persona) {
+    this.persona = persona;
+  }
 
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
+  public int getDni() {
+    return dni;
+  }
 
-    public int getCodRefuerzo() {
-        return codRefuerzo;
-    }
+  public void setDni(int dni) {
+    this.dni = dni;
+  }
 
-    public void setCodRefuerzo(int codRefuerzo) {
-        this.codRefuerzo = codRefuerzo;
-    }
+  public int getCodRefuerzo() {
+    return codRefuerzo;
+  }
 
-    public String getFechaHoraCita() {
-        return FechaHoraCita;
-    }
+  public void setCodRefuerzo(int codRefuerzo) {
+    this.codRefuerzo = codRefuerzo;
+  }
 
-    public void setFechaHoraCita(String FechaHoraCita) {
-        this.FechaHoraCita = FechaHoraCita;
-    }
+  public LocalDateTime getFechaHoraCita() {
+    return FechaHoraCita;
+  }
 
-    public int getCentroVacunacion() {
-        return centroVacunacion;
-    }
+  public void setFechaHoraCita(LocalDateTime FechaHoraCita) {
+    this.FechaHoraCita = FechaHoraCita;
+  }
 
-    public void setCentroVacunacion(int centroVacunacion) {
-        this.centroVacunacion = centroVacunacion;
-    }
+  public int getCentroVacunacion() {
+    return centroVacunacion;
+  }
 
-    public LocalDateTime getFechaHoraColoca() {
-        return fechaHoraColoca;
-    }
+  public void setCentroVacunacion(int centroVacunacion) {
+    this.centroVacunacion = centroVacunacion;
+  }
 
-    public void setFechaHoraColoca(LocalDateTime fechaHoraColoca) {
-        this.fechaHoraColoca = fechaHoraColoca;
-    }
+  public LocalDateTime getFechaHoraColoca() {
+    return fechaHoraColoca;
+  }
 
-    public Vacuna getDosis() {
-        return dosis;
-    }
+  public void setFechaHoraColoca(LocalDateTime fechaHoraColoca) {
+    this.fechaHoraColoca = fechaHoraColoca;
+  }
 
-    public void setDosis(Vacuna dosis) {
-        this.dosis = dosis;
-    }
+  public Vacuna getDosis() {
+    return dosis;
+  }
 
-    public boolean isCitaEstado() {
-        return citaEstado;
-    }
+  public void setDosis(Vacuna dosis) {
+    this.dosis = dosis;
+  }
 
-    public void setCitaEstado(boolean citaEstado) {
-        this.citaEstado = citaEstado;
-    }
-    
-    
-    
-    
+  public boolean isCitaEstado() {
+    return citaEstado;
+  }
+
+  public void setCitaEstado(boolean citaEstado) {
+    this.citaEstado = citaEstado;
+  }
+
 }
